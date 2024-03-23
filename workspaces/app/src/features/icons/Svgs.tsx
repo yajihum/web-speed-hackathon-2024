730c7155f694591fde4440c05d45427def7cafd4
yajium <yajiuma720@gmail.com>
2024-03-23 16:06:35 +0900

    mui icons削除で差分出たところの修正とshimの削除

--- workspaces/app/src/features/icons/Svgs.tsx
+++ workspaces/app/src/features/icons/Svgs.tsx
@@ -3,6 +3,7 @@ export const ArrowBack = () => {
     <svg
       aria-hidden='true'
       data-testid='ArrowBackIcon'
+      fill='currentColor'
       focusable='false'
       style={{ color: '#202020', height: '32px', width: '32px' }}
       viewBox='0 0 24 24'
@@ -18,6 +19,7 @@ export const Search = () => {
       aria-hidden='true'
       className='MuiSvgIcon-root MuiSvgIcon-fontSizeMedium css-i4bv87-MuiSvgIcon-root'
       data-testid='SearchIcon'
+      fill='currentColor'
       focusable='false'
       style={{ color: 'rgb(255, 255, 255)', height: '24px', width: '24px' }}
       viewBox='0 0 24 24'
@@ -31,22 +33,7 @@ export const Favorite = () => {
   return (
     <svg
       aria-hidden='true'
-      className='MuiSvgIcon-root MuiSvgIcon-fontSizeMedium css-i4bv87-MuiSvgIcon-root'
-      data-testid='FavoriteBorderIcon'
-      focusable='false'
-      style={{ color: 'rgb(192, 192, 192)', height: '24px', width: '24px' }}
-      viewBox='0 0 24 24'
-    >
-      <path d='M16.5 3c-1.74 0-3.41.81-4.5 2.09C10.91 3.81 9.24 3 7.5 3 4.42 3 2 5.42 2 8.5c0 3.78 3.4 6.86 8.55 11.54L12 21.35l1.45-1.32C18.6 15.36 22 12.28 22 8.5 22 5.42 19.58 3 16.5 3m-4.4 15.55-.1.1-.1-.1C7.14 14.24 4 11.39 4 8.5 4 6.5 5.5 5 7.5 5c1.54 0 3.04.99 3.57 2.36h1.87C13.46 5.99 14.96 5 16.5 5c2 0 3.5 1.5 3.5 3.5 0 2.89-3.14 5.74-7.9 10.05' />
-    </svg>
-  );
-};
-
-export const FavoriteBorder = () => {
-  return (
-    <svg
-      aria-hidden='true'
-      data-testid='FavoriteIcon'
+      fill='currentColor'
       focusable='false'
       style={{ color: 'rgb(247, 102, 133)', height: '24px', width: '24px' }}
       viewBox='0 0 24 24'
@@ -56,11 +43,27 @@ export const FavoriteBorder = () => {
   );
 };
 
+export const FavoriteBorder = () => {
+  return (
+    <svg
+      aria-hidden='true'
+      data-testid='FavoriteIcon'
+      fill='currentColor'
+      focusable='false'
+      style={{ color: 'rgb(192, 192, 192)', height: '24px', width: '24px' }}
+      viewBox='0 0 24 24'
+    >
+      <path d='M16.5 3c-1.74 0-3.41.81-4.5 2.09C10.91 3.81 9.24 3 7.5 3 4.42 3 2 5.42 2 8.5c0 3.78 3.4 6.86 8.55 11.54L12 21.35l1.45-1.32C18.6 15.36 22 12.28 22 8.5 22 5.42 19.58 3 16.5 3m-4.4 15.55-.1.1-.1-.1C7.14 14.24 4 11.39 4 8.5 4 6.5 5.5 5 7.5 5c1.54 0 3.04.99 3.57 2.36h1.87C13.46 5.99 14.96 5 16.5 5c2 0 3.5 1.5 3.5 3.5 0 2.89-3.14 5.74-7.9 10.05' />
+    </svg>
+  );
+};
+
 export const Close = () => {
   return (
     <svg
       aria-hidden='true'
       data-testid='CloseIcon'
+      fill='currentColor'
       focusable='false'
       style={{ color: 'rgb(255, 255, 255)', height: '32px', width: '32px' }}
       viewBox='0 0 24 24'
@@ -75,6 +78,7 @@ export const NavigateNext = () => {
     <svg
       aria-hidden='true'
       data-testid='NavigateNextIcon'
+      fill='currentColor'
       focusable='false'
       style={{ color: 'rgb(222, 145, 29)', height: '32px', width: '32px' }}
       viewBox='0 0 24 24'
